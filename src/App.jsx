0b9372c99031a18ec26a27c25f996c08c485acfc
Fabian Beltran <fabianbeltran1224@gmail.com>
2024-01-26 18:42:19 -0500

    add card to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import { useState } from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 import useSWR from 'swr'
 import axios from 'axios'
 import './App.css'
@@ -13,9 +20,33 @@ function App() {
 
   if (isLoading) return <div>loading...</div>
 
-  return <div> {data.results.map(
-    (item) => <li key={item.id}> {item.id} - {item.name} {item.status} {item.species} </li>
-  )}!</div>
+  return (<Stack spacing={2}>
+
+    {data.results.map(
+      (item) => <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt={item.name + " image"}
+          height="140"
+          image={item.image}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {item.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {item.origin.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {item.species}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Favorite</Button>
+        </CardActions>
+      </Card>
+    )}!
+  </Stack>)
 }
 
 export default App

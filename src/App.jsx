9f3b214c4694fe689a239a2b4c37dde25a51b94e
Fabian Beltran <fabianbeltran1224@gmail.com>
2024-01-26 18:59:43 -0500

    adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -6,10 +5,19 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 import useSWR from 'swr'
 import axios from 'axios'
-import './App.css'
 
+import { useState } from 'react'
+
+import './App.css'
 
 function App() {
   let url = 'https://rickandmortyapi.com/api/character/'
@@ -20,33 +28,67 @@ function App() {
 
   if (isLoading) return <div>loading...</div>
 
-  return (<Stack spacing={2}>
+  return (
+    <>
 
-    {data.results.map(
-      (item) => <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          alt={item.name + " image"}
-          height="140"
-          image={item.image}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {item.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {item.origin.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {item.species}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Favorite</Button>
-        </CardActions>
-      </Card>
-    )}!
-  </Stack>)
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Stack spacing={2}>
+        {
+          data.results.map((item, index) => {
+            return (
+              <div key={index}>
+                {styledCard(item)}
+              </div>
+            )
+          })
+        }
+
+      </Stack>
+
+    </>
+  )
+}
+
+function styledCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={props.image}
+        alt={props.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.species}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  )
 }
 
 export default App

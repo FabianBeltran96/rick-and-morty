1928dc6f045a11313541a8453131083b59503d02
Fabian Beltran <fabianbeltran1224@gmail.com>
2024-01-26 18:35:25 -0500

    fetching data api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import useSWR from 'swr'
+import axios from 'axios'
 import './App.css'
 
+
 function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+  let url = 'https://rickandmortyapi.com/api/character/'
+  const fetcher = url => axios.get(url).then(res => res.data)
+  const { data, error, isLoading } = useSWR(url, fetcher)
+
+  if (error) return <div>failed to load</div>
+
+  if (isLoading) return <div>loading...</div>
+
+  return <div> {data.results.map(
+    (item) => <li key={item.id}> {item.id} - {item.name} {item.status} {item.species} </li>
+  )}!</div>
 }
 
 export default App

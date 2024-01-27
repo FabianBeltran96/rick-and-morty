3dd086378e0d12ae2d373368c46c822ea7339941
Fabian Beltran <fabianbeltran1224@gmail.com>
2024-01-26 19:44:46 -0500

    adjust styles

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
+import Container from '@mui/material/Container';
+
 import useSWR from 'swr'
 import axios from 'axios'
 
@@ -19,57 +21,9 @@ import { useState } from 'react'
 
 import './App.css'
 
-function App() {
-  let url = 'https://rickandmortyapi.com/api/character/'
-  const fetcher = url => axios.get(url).then(res => res.data)
-  const { data, error, isLoading } = useSWR(url, fetcher)
-
-  if (error) return <div>failed to load</div>
-
-  if (isLoading) return <div>loading...</div>
-
-  return (
-    <>
-
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Stack spacing={2}>
-        {
-          data.results.map((item, index) => {
-            return (
-              <div key={index}>
-                {styledCard(item)}
-              </div>
-            )
-          })
-        }
-
-      </Stack>
-
-    </>
-  )
-}
-
 function styledCard(props) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <>
       <CardMedia
         component="img"
         height="140"
@@ -87,8 +41,64 @@ function styledCard(props) {
       <CardActions>
         <Button size="small">Learn More</Button>
       </CardActions>
-    </Card>
+
+    </>)
+}
+
+function App() {
+  let url = 'https://rickandmortyapi.com/api/character/'
+  const fetcher = url => axios.get(url).then(res => res.data)
+  const { data, error, isLoading } = useSWR(url, fetcher)
+
+  if (error) return <div>failed to load</div>
+
+  if (isLoading) return <div>loading...</div>
+
+  return (
+    <>
+      <Container sx={{ padding: 0 }} >
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+              >
+                MUI
+              </Typography>
+
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Stack spacing={3}>
+          {
+            data.results.map((item, index) => {
+              return (
+                <Card key={index}>
+                  {styledCard(item)}
+                </Card>
+              )
+            })
+          }
+
+        </Stack>
+
+      </Container>
+    </>
   )
 }
+
+
 
 export default App
